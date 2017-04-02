0a59fb9efb7e62692e580d594ec025b4ad1f2ddc
Александр Сабри <sabry@Aleksandrs-MacBook-Pro.local>
2017-04-02 22:31:55 +0300

    Second commit

---
+++ socialnetwork/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  socialnetwork
+//
+//  Created by Александр Сабри on 02.04.17.
+//  Copyright © 2017 sabry. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
